fdb5c60649f6a2617d0ecbd2209a8634728e44e0
Andrii <andriutsa1@ukr.net>
2022-09-23 02:25:46 +0200

    DEBUG created new File

--- src/com/javapro/lesson10/FileLogger.java
+++ src/com/javapro/lesson10/FileLogger.java
@@ -20,38 +20,21 @@ public class FileLogger implements FileLoggerAvaible {
 
   @Override
   public void debug(String str) {
-    if (createFile().length() <= loader.load().getMaxSizeByte()) {
       writeFile(str, LogginLevel.DEBUG);
-    } else {
-      createFile();
-      writeFile(str, LogginLevel.DEBUG);
-    }
-    System.out.printf("[%s][%s] Сообщение: [%s]\n", formatter.format(date), LogginLevel.DEBUG, str);
-    System.out.println(createFile().length());
-
   }
 
   @Override
   public void info(String str) {
-    if (createFile().length() <= loader.load().getMaxSizeByte()) {
       writeFile(str, LogginLevel.INFO);
-    } else {
-      createFile();
-      writeFile(str, LogginLevel.INFO);
-    }
-    System.out.printf("[%s][%s][%s]\n", formatter.format(date), LogginLevel.INFO, str);
-    System.out.println(createFile().length());
-
   }
 
   private File createFile() {
     File file = new File(loader.load().getNameFile() + loader.load().getNameFormat());
-    if (file.length() <= loader.load().getMaxSizeByte()) {
-      return file;
-    } else {
-      return new File(
+    if (file.length() >= loader.load().getMaxSizeByte()) {
+      file = new File(
           loader.load().getNameFile() + formatter.format(date) + loader.load().getNameFormat());
     }
+    return file;
   }
 
 
