f12c113867bf43bfc421ff79000c7e91efad8636
Andrey0201 <andriutsa1@ukr.net>
2022-09-27 03:06:36 +0300

    Draft 6 TASK

--- src/com/javapro/lesson10/FileLoggerConfigurationLoader.java
+++ src/com/javapro/lesson10/FileLoggerConfigurationLoader.java
@@ -10,51 +10,41 @@ public class FileLoggerConfigurationLoader {
 
 
     public FileLoggerConfiguration load() {
+
+        return parsConfigurationString();
+    }
+
+    private String getStringFromFile() {
+        StringBuilder s = new StringBuilder();
+        String FILE_PATH = ".\\src\\com\\javapro\\lesson10\\config.txt";
+        try (FileReader reader = new FileReader(
+                FILE_PATH)) {
+            int c;
+            while ((c = reader.read()) != -1) {
+                s.append((char) c);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return s.toString();
+    }
+
+
+    private FileLoggerConfiguration parsConfigurationString() {
         FileLoggerConfiguration configuration = new FileLoggerConfiguration();
-        try (InputStream input = new FileInputStream("my.config.properties")) {
-            Properties prop = new Properties();
-            prop.load(input);
-            configuration.setNameFile(prop.getProperty("file"));
-            configuration.setLevel(prop.getProperty("logging_level"));
-            configuration.setMaxSizeByte(prop.getProperty("max_size"));
-            configuration.setNameFormat(prop.getProperty("format"));
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
+        String[] str = getStringFromFile().split("\n");
+        for (String s : str) {
+            int index = s.indexOf(":");
+            String key = s.substring(0, index);
+            String value = s.substring(index + 1);
+            switch (key) {
+                case "FILE" -> configuration.setNameFile(value.trim());
+                case "LEVEL" -> configuration.setLevel(value.trim());
+                case "MAX-SIZE" -> configuration.setMaxSizeByte(value.trim());
+                case "FORMAT" -> configuration.setNameFormat(value.trim());
+            }
         }
         return configuration;
     }
-
-//    private String getStringFromFile() {
-//        StringBuilder s = new StringBuilder();
-//        String FILE_PATH = ".\\src\\com\\javapro\\lesson10\\config.txt";
-//        try (FileReader reader = new FileReader(
-//                FILE_PATH)) {
-//            int c;
-//            while ((c = reader.read()) != -1) {
-//                s.append((char) c);
-//            }
-//        } catch (IOException e) {
-//            System.out.println(e.getMessage());
-//        }
-//        return s.toString();
-//    }
-//
-//
-//    private FileLoggerConfiguration parsConfigurationString() {
-//        FileLoggerConfiguration configuration = new FileLoggerConfiguration();
-//        String[] str = getStringFromFile().split("\n");
-//        for (String s : str) {
-//            int index = s.indexOf(":");
-//            String key = s.substring(0, index);
-//            String value = s.substring(index + 1);
-//            switch (key) {
-//                case "FILE" -> configuration.setNameFile(value.trim());
-//                case "LEVEL" -> configuration.setLevel(value.trim());
-//                case "MAX-SIZE" -> configuration.setMaxSizeByte(value.trim());
-//                case "FORMAT" -> configuration.setNameFormat(value.trim());
-//            }
-//        }
-//        return configuration;
-//    }
 
 }
