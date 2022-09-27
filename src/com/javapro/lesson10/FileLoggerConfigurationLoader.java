d834bdb18a598b2c136a0ff7f9336ca888123df4
Andrey0201 <andriutsa1@ukr.net>
2022-09-27 03:02:20 +0300

    Draft 6 TASK

--- src/com/javapro/lesson10/FileLoggerConfigurationLoader.java
+++ src/com/javapro/lesson10/FileLoggerConfigurationLoader.java
@@ -1,13 +1,11 @@
 package com.javapro.lesson10;
 
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class FileLoggerConfigurationLoader {
-
 
     public FileLoggerConfiguration load() {
         FileLoggerConfiguration configuration = new FileLoggerConfiguration();
@@ -24,37 +22,5 @@ public class FileLoggerConfigurationLoader {
         return configuration;
     }
 
-//    private String getStringFromFile() {
-//        StringBuilder s = new StringBuilder();
-//        String FILE_PATH = ".\\src\\com\\javapro\\lesson10\\config.txt";
-//        try (FileReader reader = new FileReader(
-//                FILE_PATH)) {
-//            int c;
-//            while ((c = reader.read()) != -1) {
-//                s.append((char) c);
-//            }
-//        } catch (IOException e) {
-//            System.out.println(e.getMessage());
-//        }
-//        return s.toString();
-//    }
-//
-//
-//    private FileLoggerConfiguration parsConfigurationString() {
-//        FileLoggerConfiguration configuration = new FileLoggerConfiguration();
-//        String[] str = getStringFromFile().split("\n");
-//        for (String s : str) {
-//            int index = s.indexOf(":");
-//            String key = s.substring(0, index);
-//            String value = s.substring(index + 1);
-//            switch (key) {
-//                case "FILE" -> configuration.setNameFile(value.trim());
-//                case "LEVEL" -> configuration.setLevel(value.trim());
-//                case "MAX-SIZE" -> configuration.setMaxSizeByte(value.trim());
-//                case "FORMAT" -> configuration.setNameFormat(value.trim());
-//            }
-//        }
-//        return configuration;
-//    }
 
 }
