75b3a42fdfdb4fe8599506fb7d3d757f669b3a7f
Andrii <andriutsa1@ukr.net>
2022-09-30 09:39:44 +0200

    Edit

--- src/com/javapro/lesson10/model/fileconfig/FileLoggerConfigurationLoader.java
+++ src/com/javapro/lesson10/model/fileconfig/FileLoggerConfigurationLoader.java
@@ -3,25 +3,29 @@ package com.javapro.lesson10.model.fileconfig;
 import com.javapro.lesson10.api.ILoggerConfigurationLoader;
 
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.Reader;
 import java.util.Properties;
 
 public class FileLoggerConfigurationLoader implements ILoggerConfigurationLoader {
 
-    @Override
-    public FileLoggerConfiguration load() {
-        FileLoggerConfiguration configuration = new FileLoggerConfiguration();
-        try (InputStream input = new FileInputStream("my.config.properties")) {
-            Properties prop = new Properties();
-            prop.load(input);
-            configuration.setNameFile(prop.getProperty("file"));
-            configuration.setLevel(prop.getProperty("logging_level"));
-            configuration.setMaxSizeByte(prop.getProperty("max_size"));
-            configuration.setNameFormat(prop.getProperty("format"));
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-        return configuration;
+  @Override
+  public FileLoggerConfiguration load() {
+    FileLoggerConfiguration configuration = new FileLoggerConfiguration();
+
+    try {
+      FileReader input = new FileReader("my.config.properties");
+      Properties prop = new Properties();
+      prop.load(input);
+      configuration.setNameFile(prop.getProperty("file"));
+      configuration.setLevel(prop.getProperty("logging_level"));
+      configuration.setMaxSizeByte(prop.getProperty("max_size"));
+      configuration.setNameFormat(prop.getProperty("format"));
+    } catch (IOException ex) {
+      System.out.println(ex.getMessage());
     }
+    return configuration;
+  }
 }
